918c232d2604efd145bc2d65dc47e15d5ed4706d
Tevezx <teveztwitch@gmail.com>
2025-07-04 08:53:10 -0300

    Estruturas de repetição

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/aula05EstruturasCondicionais06.java
@@ -0,0 +1,56 @@
+package academy.devdojo.maratonajava.introducao;
+
+//importando a bibliteca para utilizar scanner e permitir a entrada de dados pelo usuario
+import java.util.Scanner;
+
+public class aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+
+        //Inicializando o Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Dados os valores de 1 a 7, imprima se é dia util ou final de semana
+        // considerando 1 como domingo
+        System.out.println("Digite o dia: ");
+        int dia = scanner.nextInt();
+
+        switch (dia){
+            case 1:
+                System.out.println("Domingo - Final de semana");
+                break;
+            case 2:
+                System.out.println("Segunda - Dia útil");
+                break;
+            case 3:
+                System.out.println("Terça-Feira - Dia útil");
+                break;
+            case 4:
+                System.out.println("Quarta-Feira - Dia útil");
+                break;
+            case 5:
+                System.out.println("Quinta-Feira - Dia útil");
+                break;
+            case 6:
+                System.out.println("Sexta-feira - Dia útil");
+                break;
+            case 7:
+                System.out.println("Sábado - Final de semana");
+                break;
+            default:
+                System.out.println("Digite um numero entre 1 á 7");
+                break;
+        }
+
+        // atualizacao
+        /*
+            byte day = 1;
+            // Using switch expressions (added in java 13)
+            String answer = switch (day) {
+                case 1, 7 -> "Final de semana";
+                case 2, 3, 4, 5, 6 -> "Dia útil";
+                default -> "Dia não existente";
+             };
+            System.out.println(answer);
+         */
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/aula06EstruturasRepeticao01.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula06EstruturasRepeticao01 {
+    public static void main(String[] args) {
+
+    }
+}
